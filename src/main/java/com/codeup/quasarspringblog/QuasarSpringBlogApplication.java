d6e225015e17d78c2d6c50a481dc2e8df8206d01
kennethhowell <howell.kenneth.a@gmail.com>
2021-12-16 13:57:55 -0600

    uploading basic project structure

---
+++ src/main/java/com/codeup/quasarspringblog/QuasarSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.quasarspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuasarSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuasarSpringBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/quasarspringblog/QuasarSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.quasarspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuasarSpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
