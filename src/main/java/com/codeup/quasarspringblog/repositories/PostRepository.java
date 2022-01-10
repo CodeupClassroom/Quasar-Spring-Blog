a663e99014a8b9fed5760b28a72219be82c61759
kennethhowell <howell.kenneth.a@gmail.com>
2022-01-10 10:18:53 -0600

    complete: email service addition

--- src/main/java/com/codeup/quasarspringblog/repositories/PostRepository.java
+++ src/main/java/com/codeup/quasarspringblog/repositories/PostRepository.java
@@ -1,7 +1,7 @@
-package com.codeup.quasarspringblog.controllers;
+package com.codeup.quasarspringblog.repositories;
 
+import com.codeup.quasarspringblog.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface PostRepository extends JpaRepository<Post, Long> {
--- src/main/java/com/codeup/quasarspringblog/repositories/UserRepository.java
+++ src/main/java/com/codeup/quasarspringblog/repositories/UserRepository.java
@@ -1,5 +1,6 @@
-package com.codeup.quasarspringblog.controllers;
+package com.codeup.quasarspringblog.repositories;
 
+import com.codeup.quasarspringblog.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
