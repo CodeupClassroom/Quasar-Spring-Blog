3ffe31de8ec4cf35fcbe26fbba4f92c688561c36
kennethhowell <howell.kenneth.a@gmail.com>
2022-01-17 16:16:32 -0600

    file uploaded code written and walked through

---
+++ src/main/java/com/codeup/quasarspringblog/controllers/FileUploadController.java
@@ -0,0 +1,51 @@
+package com.codeup.quasarspringblog.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletContext;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+@Controller
+public class FileUploadController {
+    @Autowired
+    ServletContext context;
+
+    @Value("${file-upload-path}")
+    private String uploadPath;
+
+    @GetMapping("/fileupload")
+    public String showUploadFileForm() {
+        return "fileupload";
+    }
+
+    @PostMapping("/fileupload")
+    public String saveFile(
+            @RequestParam(name = "file") MultipartFile uploadedFile,
+            Model model
+    ) {
+        String filename = uploadedFile.getOriginalFilename();
+        String realPath = context.getRealPath("resources/uploaded");
+        String contextPath = context.getContextPath();
+        System.out.println("'Real path' test : " + realPath);
+        System.out.println("'Context path' rest:" + contextPath);
+        String filepath = Paths.get(uploadPath, filename).toString();
+        File destinationFile = new File(filepath);
+        try {
+            uploadedFile.transferTo(destinationFile);
+            model.addAttribute("message", "File successfully uploaded!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            model.addAttribute("message", "Oops! Something went wrong! " + e);
+        }
+        return "fileupload";
+    }
+}
---
+++ src/test/java/com/codeup/quasarspringblog/PostsIntegrationTests.java
@@ -0,0 +1,72 @@
+package com.codeup.quasarspringblog;
+
+
+import com.codeup.quasarspringblog.models.User;
+import com.codeup.quasarspringblog.repositories.PostRepository;
+import com.codeup.quasarspringblog.repositories.UserRepository;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.servlet.http.HttpSession;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = QuasarSpringBlogApplication.class)
+@AutoConfigureMockMvc
+public class PostsIntegrationTests {
+
+    private User testUser;
+    private HttpSession httpSession;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    UserRepository userDao;
+
+    @Autowired
+    PostRepository postDao;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Before
+    public void setup() throws Exception{
+        testUser = userDao.findByUsername("testUser");
+
+        // Creates the test user if not exists
+        if(testUser == null){
+            User newUser = new User();
+            newUser.setUsername("testUser");
+            newUser.setPassword(passwordEncoder.encode("pass"));
+            newUser.setEmail("testUser@codeup.com");
+            testUser = userDao.save(newUser);
+        }
+
+        // Throws a Post request to /login and expect a redirection to the Ads index page after being logged in
+        httpSession = this.mvc.perform(post("/login").with(csrf())
+                        .param("username", "testUser")
+                        .param("password", "pass"))
+                .andExpect(status().is(HttpStatus.FOUND.value()))
+                .andExpect(redirectedUrl("/posts"))
+                .andReturn()
+                .getRequest()
+                .getSession();
+    }
+
+}
+
+
+
+
